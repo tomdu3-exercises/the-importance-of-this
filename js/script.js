544454c94f8448b1c8f9d7a6c7d48b87cd224b48
Tomislav Dukez <tomdu3@ymail.com>
2023-04-30 08:57:46 +0000

    box classes trigger

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-$('button').mouseenter(function() {
-    $(this).removeClass('makeRed').addClass('makeBlue');
-});
-
-$('button').mouseleave(function() {
-    $(this).removeClass('makeBlue').addClass('makeRed');
+$(document).ready(function() {
+    $('.box').on('click', function() {
+        var classNames = $(this).attr('class').split(' ');
+        $('.' + classNames[1]).css('background-color', 'red')
+    })
 })
